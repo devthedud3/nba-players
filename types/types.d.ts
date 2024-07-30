8778f57b7d74bcfa0cfe568a70e3b7f09dd21cae
corey <corey.cushnie@gmail.com>
2024-07-30 11:40:39 -0400

    build types

---
+++ types/types.d.ts
@@ -0,0 +1,11 @@
+interface Player {
+  firstName: string;
+  lastName: string;
+  from: string;
+  to: string;
+  position: string;
+  height: string;
+  weight: string;
+  DOB: string;
+  college: string;
+}
