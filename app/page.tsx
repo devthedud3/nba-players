37f90d3fd3649c5d846f26502e5bbf66470326c6
corey <corey.cushnie@gmail.com>
2024-08-02 16:51:17 -0400

    build page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-12 space-y-4">
-      <Teams />
+    <div className="flex min-h-screen flex-col p-12 space-y-4">
       <News />
-      <Image alt="nba" height={50} width={50} src="/nba-1.png" />
+      <Teams />
       <h1 className="text-xl font-extrabold">NBA Players</h1>
       <Players />
-    </main>
+    </div>
   );
 }
