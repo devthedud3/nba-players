173345ddf79f7df89c1fdfb4ff81b51d78419318
corey <corey.cushnie@gmail.com>
2024-08-14 14:02:25 -0400

    build news

--- components/News.tsx
+++ components/News.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { LoadingAnimation } from "./LoadingAnimation";
+import Slider from "./Slider";
 
 type Props = {};
 
@@ -33,48 +34,17 @@ export default function News({}: Props) {
     <section className="flex flex-col gap-10">
       {loading ? (
         <div className="grid grid-cols-5 gap-6">
-          <div className="col-span-4 h-[550px]">
+          <div className="col-span-4 h-[675px]">
             <LoadingAnimation />
           </div>
-          <div className="animate-pulse delay-100 h-screen bg-gray-300 col-span-1" />
+          <div className="animate-pulse delay-100 bg-gray-300 col-span-1" />
         </div>
       ) : (
         <div className="grid grid-cols-5 gap-6">
           <div className="col-span-4 space-y-6">
-            {articles.slice(0, 5).map((article: any, index) => (
-              <article key={index} className="relative bg-black">
-                <div className="flex justify-end overflow-hidden">
-                  <div className="fade-effect translate-x-56">
-                    <Image
-                      className="animation-pulse "
-                      src={article.image}
-                      alt={article.title}
-                      height={1300}
-                      width={1000}
-                      priority
-                      style={{ height: "100%" }}
-                    />
-                  </div>
-                </div>
-                <div className="absolute h-auto inset-0 container-p w-96 text-white flex flex-col justify-center gap-6">
-                  <div className="space-y-2">
-                    <h2 className={`${oswald.className} text-3xl font-black`}>
-                      {article.title.toLocaleUpperCase()}
-                    </h2>
-                    <p className="text-xs">{article.description}</p>
-                  </div>
-                  <Link
-                    href={`https://nba.com${article.link}`}
-                    className=" border border-white w-fit p-2 "
-                    target="_blank"
-                  >
-                    Read more
-                  </Link>
-                </div>
-              </article>
-            ))}
+            <Slider articles={articles} />
           </div>
-          <div className="col-span-1 border h-fit bg-white">
+          <div className="col-span-1 border h-auto bg-white">
             <div className={` p-4`}>
               <h3
                 className={`${oswald.className} font-semibold text-xl pb-2 border-b`}
