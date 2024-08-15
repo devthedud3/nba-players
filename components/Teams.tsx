48c85d51024bd290a198f36c8a1282d9edca1e3d
corey <corey.cushnie@gmail.com>
2024-08-15 15:43:20 -0400

    optimize for mobile

--- components/Teams.tsx
+++ components/Teams.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 type Props = {};
@@ -23,18 +24,26 @@ export default function Teams({}: Props) {
   if (error) return <div>{error}</div>;
 
   return (
-    <section className="flex flex-col lg:gap-10 p-2 mx-4 lg:m-0 ">
-      <h5 className="text-2xl font-black border-b p-2">Teams</h5>
+    <section className="flex bg-white flex-col lg:gap-10 p-2 mx-4 lg:m-0 ">
+      <h5 className="text-2xl font-black p-2 border-b">Teams</h5>
 
       {loading ? (
         <p>Loading News...</p>
       ) : (
-        <div className="grid grid-cols-2 lg:grid-cols-5 ">
+        <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
           {teams.map((team: any, index) => (
-            <div className="flex items-center gap-2" key={index}>
+            <Link
+              className="transition flex p-1 items-center border-2 border-white hover:border-black rounded-lg gap-2 cursor-pointer"
+              key={index}
+              href={`https://nba.com/${team.name
+                .toLocaleLowerCase()
+                .split(" ")
+                .at(-1)}`}
+              target="_blank"
+            >
               <Image alt={team.name} src={team.image} height={50} width={50} />
-              <p className="text-xs">{team.name} </p>
-            </div>
+              <p className="text-[0.65rem]">{team.name} </p>
+            </Link>
           ))}
         </div>
       )}
