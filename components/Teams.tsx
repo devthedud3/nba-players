87410d0ce0cd4d314b9df1cfc0451d65527e1802
corey <corey.cushnie@gmail.com>
2024-08-15 11:08:21 -0400

    optimize for mobile

--- components/Teams.tsx
+++ components/Teams.tsx
@@ -23,13 +23,13 @@ export default function Teams({}: Props) {
   if (error) return <div>{error}</div>;
 
   return (
-    <section className="flex flex-col gap-10">
+    <section className="flex flex-col lg:gap-10 p-2 mx-4 lg:m-0 ">
       <h5 className="text-2xl font-black border-b p-2">Teams</h5>
 
       {loading ? (
         <p>Loading News...</p>
       ) : (
-        <div className="grid grid-cols-5">
+        <div className="grid grid-cols-2 lg:grid-cols-5 ">
           {teams.map((team: any, index) => (
             <div className="flex items-center gap-2" key={index}>
               <Image alt={team.name} src={team.image} height={50} width={50} />
