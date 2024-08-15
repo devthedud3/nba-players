a94f05838f0501e81576a5af377623614b54e00a
corey <corey.cushnie@gmail.com>
2024-08-15 11:08:06 -0400

    optimize for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { ShoppingBasketIcon } from "@/assets/AppIcons";
+import { AppIcon } from "@/assets/AppIcons";
 
 import Link from "next/link";
 import React from "react";
@@ -10,8 +10,8 @@ export default function Header({}: Props) {
   return (
     <header className="sticky top-0 z-10 flex h-14 items-center justify-between border-b bg-[--background] shadow-sm container-p">
       <Link href="/" className="flex items-center gap-2" prefetch={false}>
-        <ShoppingBasketIcon />
-        <span className="text-xl font-bold">Stats Corner</span>
+        <AppIcon icon="logo" />
+        <span className="text-xl font-bold">NBA Corner</span>
       </Link>
       <SearchBar icon />
     </header>
